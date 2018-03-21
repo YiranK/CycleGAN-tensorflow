a14d361f53893016031840d532ff54986360fe34
YiranK <kangyiran1996@gmail.com>
2018-03-21 22:26:42 +0800

    fix mask_A, change to not multiply mask

--- model.py
+++ model.py
@@ -46,21 +46,27 @@ class cyclegan(object):
                                          self.input_c_dim + self.output_c_dim],
                                         name='real_A_and_B_images') # real A is concated by syndata and mask
 
+        self.real_A_with_mask = self.real_data[:, :, :, :self.input_c_dim] # RGB A with its Mask
         self.real_A = self.real_data[:, :, :, :self.input_c_dim-1]
         self.real_B = self.real_data[:, :, :, self.input_c_dim:self.input_c_dim + self.output_c_dim]
-        self.mask_A = tf.expand_dims(self.real_data[:,:,:,self.input_c_dim-1],3) # the last channel of A is mask
+        self.mask_A = tf.expand_dims(self.real_data[:,:,:,self.input_c_dim],3) # the last channel of A is mask
 
-        self.fake_B = self.generator(self.real_A, self.options, False, name="generatorA2B")
-        self.fake_B_mul_mask = tf.multiply(self.fake_B, self.mask_A)+tf.multiply(self.real_A, 1-self.mask_A)
-        self.fake_A_ = self.generator(self.fake_B_mul_mask, self.options, False, name="generatorB2A") # fake_B -> fake_B_mul_mask
-        print self.input_c_dim,self.input_c_dim+self.output_c_dim
-        print self.fake_B_mul_mask, self.fake_A_, self.real_B, self.real_A, self.mask_A
+        self.fake_B = self.generator(self.real_A_with_mask, self.options, False, name="generatorA2B")
+        #self.fake_B_mul_mask = tf.multiply(self.fake_B, self.mask_A)+tf.multiply(self.real_A, 1-self.mask_A)
+        self.fake_A_ = self.generator(self.fake_B, self.options, False, name="generatorB2A") # fake_B -> fake_B_mul_mask -> fake_B
+        #print self.input_c_dim,self.input_c_dim+self.output_c_dim
+        #print self.fake_B_mul_mask, self.fake_A_, self.real_B, self.real_A, self.mask_A
+
         self.fake_A = self.generator(self.real_B, self.options, True, name="generatorB2A")
-        self.fake_B_ = self.generator(self.fake_A, self.options, True, name="generatorA2B")
+        shape_A = tf.shape(self.fake_A)
+        self.all_1_mask = tf.ones([shape_A[0], shape_A[1], shape_A[2], 1])
+        self.fake_A_with_all_1_mask = tf.concat([self.fake_A, self.all_1_mask],3)
+        print self.real_data[:, :, :, self.input_c_dim], self.all_1_mask, self.fake_A_with_all_1_mask
+        self.fake_B_ = self.generator(self.fake_A_with_all_1_mask, self.options, True, name="generatorA2B") # fake_A -> fake_A_with_all_1_mask
 
-        self.fake_B_cat_mask = tf.concat([self.fake_B, self.mask_A], 3)
-        self.DB_fake = self.discriminator(self.fake_B_mul_mask, self.options, reuse=False, name="discriminatorB") # fake_B -> fake_B_cat_mask
-        self.DA_fake = self.discriminator(self.fake_A, self.options, reuse=False, name="discriminatorA")
+        #self.fake_B_cat_mask = tf.concat([self.fake_B, self.mask_A], 3)
+        self.DB_fake = self.discriminator(self.fake_B, self.options, reuse=False, name="discriminatorB") # fake_B -> fake_B_cat_mask > fake_B
+        self.DA_fake = self.discriminator(self.fake_A, self.options, reuse=False, name="discriminatorA") # fake_A channel = 3
         self.g_loss_a2b = self.criterionGAN(self.DB_fake, tf.ones_like(self.DB_fake)) \
             + self.L1_lambda * abs_criterion(self.real_A, self.fake_A_) \
             + self.L1_lambda * abs_criterion(self.real_B, self.fake_B_)
@@ -74,7 +80,7 @@ class cyclegan(object):
 
         self.fake_A_sample = tf.placeholder(tf.float32,
                                             [None, self.image_size, self.image_size,
-                                             self.input_c_dim-1], name='fake_A_sample')
+                                             self.input_c_dim-1], name='fake_A_sample') # attention to -1
         self.fake_B_sample = tf.placeholder(tf.float32,
                                             [None, self.image_size, self.image_size,
                                              self.output_c_dim], name='fake_B_sample')
@@ -111,7 +117,7 @@ class cyclegan(object):
 
         self.test_A = tf.placeholder(tf.float32,
                                      [None, self.image_size, self.image_size,
-                                      self.input_c_dim-1], name='test_A')
+                                      self.input_c_dim], name='test_A')
         self.test_B = tf.placeholder(tf.float32,
                                      [None, self.image_size, self.image_size,
                                       self.output_c_dim], name='test_B')
@@ -133,7 +139,7 @@ class cyclegan(object):
 
         init_op = tf.global_variables_initializer()
         self.sess.run(init_op)
-        self.writer = tf.summary.FileWriter("./logs", self.sess.graph)
+        self.writer = tf.summary.FileWriter("./logs/a_with_mask_0321", self.sess.graph)
 
         counter = 1
         start_time = time.time()
@@ -219,24 +225,22 @@ class cyclegan(object):
         sample_images = [load_train_data(batch_file, is_testing=True) for batch_file in batch_files]
         sample_images = np.array(sample_images).astype(np.float32)
 
-        fake_A, fake_B, fake_B_mul_mask, real_A, real_B, mask_A = self.sess.run(
-            [self.fake_A, self.fake_B, self.fake_B_mul_mask, self.real_A, self.real_B, self.mask_A],
+        fake_A, fake_B, real_A, real_B, mask_A, all_1_mask, fake_A_with_all_1_mask = self.sess.run(
+            [self.fake_A, self.fake_B, self.real_A, self.real_B, self.mask_A, self.all_1_mask, self.fake_A_with_all_1_mask],
             feed_dict={self.real_data: sample_images}
         )
-        print "fake_B", fake_B
-        print "real_A", real_A
+        print "mask_A", mask_A.shape, mask_A
+        print "all_1_mask", all_1_mask.shape, all_1_mask
+        print "fake_A_with_all_1_mask", fake_A_with_all_1_mask.shape
         save_images(fake_A, [self.batch_size, 1],
                     './{}/{:02d}_{:04d}_fakeA.jpg'.format(sample_dir, epoch, idx))
         save_images(fake_B, [self.batch_size, 1],
                     './{}/{:02d}_{:04d}_fakeB.jpg'.format(sample_dir, epoch, idx))
-        save_images(fake_B_mul_mask, [self.batch_size, 1],
-                    './{}/{:02d}_{:04d}_maskA+B.jpg'.format(sample_dir, epoch, idx))
         save_images(real_A, [self.batch_size, 1],
                     './{}/{:02d}_{:04d}_realA.jpg'.format(sample_dir, epoch, idx))
         save_images(real_B, [self.batch_size, 1],
                     './{}/{:02d}_{:04d}_realB.jpg'.format(sample_dir, epoch, idx))
-        save_images(mask_A, [self.batch_size, 1],
-                    './{}/{:02d}_{:04d}_maskA.jpg'.format(sample_dir, epoch, idx))
+
 
 
     def test(self, args):
